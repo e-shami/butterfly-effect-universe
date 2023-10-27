ed20a015fab3d28eb15d1323aceda069dffe1f54
Naveed Ahmed Khan <nav8khan@gmail.com>
2023-10-28 04:10:10 +0500

    hero and about

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import AboutUs from '@components/about'
+import Hero from '@components/hero'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-        <h1 className='text-4xl font-bold text-center text-black-800'>
-            Butterfly Effect Universe
-        </h1>
+    <div className='flex flex-col'>
+        <Hero />
+        <AboutUs />
     </div>
   )
 }

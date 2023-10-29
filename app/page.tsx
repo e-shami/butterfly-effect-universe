097b8c50987c0a1e5c871a056bd6c87738a94a34
Ehtisham Rehmat <a7rehmat@gmail.com>
2023-10-29 09:41:52 +0500

    footer responsive for landing page implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import Gallery from '@components/gallery'
-import AboutUs from '@components/about'
-import Hero from '@components/hero'
-import React from 'react'
+import Gallery from "@components/gallery";
+import AboutUs from "@components/about";
+import Hero from "@components/hero";
+import Footer from "@components/footer";
+import React from "react";
 
 export default function page() {
   return (
-    <div className='flex flex-col'>
-        <Hero />
-        <AboutUs />
+    <div className="flex flex-col">
+      <Hero />
+      <AboutUs />
       <Gallery />
+      <Footer />
     </div>
-  )
+  );
 }
